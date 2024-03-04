1488c2bd0132912c274a8bbe461e3036bb84d746
Fuka Nagata <fleur0121@icloud.com>
2024-03-03 20:28:10 -0800

    create bio component #4

---
+++ src/components/Bio/Bio.jsx
@@ -0,0 +1,11 @@
+const Bio = () => {
+  return (
+    <div>
+      <h1>Fuka Nagata</h1>
+      <p>
+        I am a student in Langara college. I was born in Japan and moved to
+        Canada in 2020. I have a strong interest in CS.
+      </p>
+    </div>
+  );
+};
