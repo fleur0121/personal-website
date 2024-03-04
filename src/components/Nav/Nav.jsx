dddd6dbf8f7a4c0c65b7224c6565530406755ff8
Fuka Nagata <fleur0121@icloud.com>
2024-03-03 20:26:28 -0800

    create nav component #1

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+const navBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>BIO</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default navBar;
