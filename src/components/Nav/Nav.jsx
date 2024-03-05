94ec4f4b5272680eab8ee284b382e756f1585432
Fuka Nagata <fleur0121@icloud.com>
2024-03-04 21:30:26 -0800

    #1 update navbar & add styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,23 @@
-const navBar = () => {
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import style from "./Nav.module.css";
+
+const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>BIO</li>
-        <li>Skills</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <Navbar className={style.navBar}>
+      <Container className={style.navBarElements}>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className={style.navBarLinks}>
+          <Nav.Link href="#bio">Bio</Nav.Link>
+          <Nav.Link href="#skills">Skills</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link href="#thanks">Special Thanks</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
 
-export default navBar;
+export default NavBar;
