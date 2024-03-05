8d42c0edecd8e625f4cb4cd31c62b93fbfc77057
Fuka Nagata <fleur0121@icloud.com>
2024-03-05 12:48:34 -0800

    updated nav bar, reday to marge #1

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,15 +7,13 @@ import style from "./Nav.module.css";
 const NavBar = () => {
   return (
     <Navbar className={style.navBar}>
-      <Container className={style.navBarElements}>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className={style.navBarLinks}>
-          <Nav.Link href="#bio">Bio</Nav.Link>
-          <Nav.Link href="#skills">Skills</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-          <Nav.Link href="#thanks">Special Thanks</Nav.Link>
-        </Nav>
-      </Container>
+      <Nav className={style.navBarElements}>
+        <Nav.Link href="#bio">Home</Nav.Link>
+        <Nav.Link href="#bio">Bio</Nav.Link>
+        <Nav.Link href="#skills">Skills</Nav.Link>
+        <Nav.Link href="#contact">Contact</Nav.Link>
+        <Nav.Link href="#thanks">Special Thanks</Nav.Link>
+      </Nav>
     </Navbar>
   );
 };
