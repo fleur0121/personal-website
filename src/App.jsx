68017085e03d6e8a067c70c747a1f783a51083a8
Fuka Nagata <fleur0121@icloud.com>
2024-03-15 23:48:08 -0700

    update path again #16

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import NavBar from "src/components/Bio";
+import NavBar from "src/components/Nav/Nav.jsx";
 import Bio from "src/components/Bio/Bio.jsx";
-import Skills from "src/components/Skills/Skills";
-import Project from "src/components/Project/Project";
-import Contact from "src/components/Contact/Contact";
+import Skills from "src/components/Skills/Skills.jsx";
+import Project from "src/components/Project/Project.jsx";
+import Contact from "src/components/Contact/Contact.jsx";
 import "./App.css";
 
 function App() {
